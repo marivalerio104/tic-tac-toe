5925f1d76b11eb2b078b807d12a5d76626af0a5b
Mariela Valerio <139282125+marivalerio104@users.noreply.github.com>
2026-02-12 20:23:10 -0600

    Add players
    Add the functionality to edit the players' names

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <>
-     <h1>Hola</h1>
-    </>
-  )
-}
+import Player from './components/Player/Player'
+import './App.css';
 
-export default App
+export default function App() {
+  return <main>
+    <div id="game-container">
+      <div id="players">
+        <Player initialName="Player 1" symbol="X" />
+        <Player initialName="Player 2" symbol="O" />
+      </div>
+
+      GAME BOARD
+    </div>
+
+    LOG
+  </main>
+}
---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './Player.css'
+
+export default function Player({ initialName, symbol }) {
+  const [name, setName] = useState(initialName);
+  const [editing, setEditing] = useState(false);
+
+  function handleChange(event) {
+    setName(event.target.value)
+  }
+
+  function handleClick() {
+    setEditing(prev => !prev)
+  }
+
+  let playerName = <span className="player-name">{name}</span>
+  if (editing) playerName = <input type="text" value={name} onChange={handleChange} />;
+
+  return <div className="player">
+    {playerName}
+    <span className="player-symbol">{symbol}</span>
+    <button onClick={handleClick}>{editing ? "Save" : "Edit"}</button>
+  </div>
+}
